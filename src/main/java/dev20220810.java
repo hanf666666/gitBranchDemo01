45427d7f6c2f44fd151667097d7e4601e39d8e23
hanf666666 <hanjing_xl@163.com>
2022-08-09 11:30:16 +0800

    dev20220810

---
+++ src/main/java/dev20220810.java
@@ -0,0 +1,11 @@
+/**
+ * to do
+ *
+ * @author Hj
+ * @date 2022/8/9
+ */
+public class dev20220810 {
+    public static void main(String[] args) {
+        System.out.println("dev20220810");
+    }
+}
