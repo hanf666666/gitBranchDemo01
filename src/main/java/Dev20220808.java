353695092e11b3a9ff39669fbf19597ca92ae16a
hanf666666 <hanjing_xl@163.com>
2022-08-09 11:14:05 +0800

    dev20220808

--- src/main/java/Dev20220808.java
+++ src/main/java/Dev20220808.java
@@ -7,5 +7,6 @@
 public class Dev20220808 {
     public static void main(String[] args) {
         System.out.println("dev2022080820220808dev20220808");
+        System.out.println("dev2022080820220808dev20220808");
     }
 }
